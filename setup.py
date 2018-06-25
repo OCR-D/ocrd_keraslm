f4378fe2ca50d6031f095f30b38a12300f6567bd
Kay-Michael Würzner <wuerzner@bbaw.de>
2018-06-25 17:28:53 +0200

    * setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Installs:
+    - ocrd-keraslm-rate
+"""
+import codecs
+
+from setuptools import setup, find_packages
+
+with codecs.open('README.md', encoding='utf-8') as f:
+    README = f.read()
+
+setup(
+    name='ocrd_keraslm',
+    version='0.0.1',
+    description='keras language model',
+    long_description=README,
+    author='Konstantin Baierer, Kay-Michael Würzner',
+    author_email='unixprog@gmail.com, wuerzner@gmail.com',
+    url='https://github.com/OCR-D/ocrd_keraslm',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'ocrd >= 0.4.0',
+        'keras',
+        'click',
+        'numpy',
+        'tensorflow',
+    ],
+    package_data={
+        '': ['*.json', '*.yml', '*.yaml'],
+    },
+    entry_points={
+        'console_scripts': [
+            'ocrd-keraslm-rate=ocrd_keraslm.scripts.run:cli',
+        ]
+    },
+)
