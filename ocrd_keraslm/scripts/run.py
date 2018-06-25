6c5c1cae4530726cfb60125657f9e0de06c3462e
Kay-Michael Würzner <wuerzner@bbaw.de>
2018-06-25 20:26:25 +0200

    * as a lib

--- ocrd_keraslm/scripts/run.py
+++ ocrd_keraslm/scripts/run.py
@@ -1,71 +1,29 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 
-import click, sys, numpy, pickle
-from keras.utils import to_categorical
-from keras.models import Sequential, load_model
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.preprocessing.sequence import pad_sequences
+import click, sys, json
+
+from ocrd_keraslm import lib
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-@click.option('-t', '--training', required=True)
 @click.option('-o', '--output', default="model")
-def train(training, output):
+@click.argument('data')
+def train(output, data):
     """Train a language model"""
 
-    #
-    # Step 1: read and encode training data
-    #
-    with open(str(training),"r") as f:
-        raw_data = f.read()
+    # train
+    rater = lib.Rater()
+    with open(str(data),"r") as f:
+        training_data = f.read()
 
-        #
-        # vocabulary
-        chars = sorted(list(set(raw_data)))
-        c_i = dict((c, i) for i, c in enumerate(chars))
-        i_c = dict((i, c) for i, c in enumerate(chars))
-        # vocabulary size
-        vocab_size = len(c_i)
-        click.echo(u'Vocabulary Size: %d' % vocab_size)
-
-        length = 5
-        step = 3
-        sequences = []
-        next_chars = []
-        with click.progressbar(range(0, len(raw_data) - length, step)) as bar:
-            for i in bar:
-                sequences.append(raw_data[i: i + length])
-                next_chars.append(raw_data[i + length])
-        click.echo(u'Sequences Size: %d' % len(sequences))
-
-        x = numpy.zeros((len(sequences), length, vocab_size), dtype=numpy.bool)
-        y = numpy.zeros((len(sequences), vocab_size), dtype=numpy.bool)
-
-        with click.progressbar(enumerate(sequences)) as bar:
-            for i, sequence in bar:
-                for t, char in enumerate(sequence):
-                    x[i, t, c_i[char]] = 1
-                    y[i, c_i[next_chars[i]]] = 1
-
-    # define model
-    model = Sequential()
-    model.add(LSTM(128, input_shape=(length, vocab_size)))
-    model.add(Dense(vocab_size, activation='softmax'))
-    click.echo(model.summary())
-
-    # compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # fit model
-    model.fit(x, y, batch_size=128, epochs=100, verbose=2)
+        rater.train(training_data)
 
     # save model and dicts
-    model.save(u"%s.h5" % output)
-    pickle.dump((c_i,i_c), open(u"%s.map" % output, mode='wb'))
+    rater.save(output)
 
 @cli.command()
 @click.option('-m', '--model', required=True)
@@ -75,12 +33,8 @@ def apply(model, mapping, text):
     """Apply a language model"""
 
     # load model
-    mdl = load_model(model)
-
-    # load mapping
-    c_i, i_c = pickle.load(open(mapping, mode="rb"))
-    vocab_size = len(c_i)
-    length = 5 
+    rater = lib.Rater()
+    rater.load(mapping,model)
 
     # read input
     in_string = u""
@@ -92,16 +46,8 @@ def apply(model, mapping, text):
     else:
         pass
 
-    encoded_buffer = []
-    for c in in_string:
-        x = numpy.zeros((1, length, vocab_size))
-        encoded = pad_sequences([encoded_buffer], maxlen=length, truncating='pre')
-        for t, i in enumerate(encoded[0]):
-            x[0, t, i] = 1.
-        pred = dict(enumerate(mdl.predict(x, verbose=0).tolist()[0]))
-        i = c_i[c]
-        click.echo("%s: %.8f" % (c, pred[i]))
-        encoded_buffer.append(i)
+    ratings = rater.rate(in_string)
+    click.echo(json.dumps(ratings))
 
 if __name__ == '__main__':
     cli()
