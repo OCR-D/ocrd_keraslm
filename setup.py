da105a8a8b68844389cd3e08307c05c9c6123350
bertsky <sachunsky@informatik.uni-leipzig.de>
2019-11-18 22:56:24 +0100

    restrict Tensorflow < 2.0, v0.3.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with codecs.open('README.md', encoding='utf-8') as f:
 
 setup(
     name='ocrd_keraslm',
-    version='0.3.1',
+    version='0.3.2',
     description='character-level language modelling in Keras',
     long_description=README,
     long_description_content_type='text/markdown',
