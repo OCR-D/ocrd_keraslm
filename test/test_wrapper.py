35d70478b5d7d03f0fe1110b88e9f1fd6e128987
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2025-04-15 22:25:23 +0200

    improve+adapt tests…
    - unittest → pytest
    - split scoring and decoding
    - add result checks/assertions
    - add mode w/ METS Server / page workers

--- test/test_wrapper.py
+++ test/test_wrapper.py
@@ -1,86 +1,98 @@
-import os, sys
-import shutil
-from unittest import TestCase, main
+import os
+import logging
 
-from ocrd.resolver import Resolver
-from ocrd_models.ocrd_page import to_xml
+from ocrd import run_processor
 from ocrd_modelfactory import page_from_file
-from ocrd_utils import MIMETYPE_PAGE, initLogging
 from ocrd_tesserocr.recognize import TesserocrRecognize
 from ocrd_keraslm.wrapper import KerasRate
 
-WORKSPACE_DIR = '/tmp/pyocrd-test-ocrd_keraslm'
 MODEL = os.getenv('OCRD_KERASLM_MODEL', 'model_dta_full.h5')
 
-class TestKerasRate(TestCase):
+def test_scoring(processor_kwargs, caplog):
+    """rate text alternative 1 on the word level"""
+    caplog.set_level(logging.DEBUG)
+    ws = processor_kwargs['workspace']
+    run_processor(
+        KerasRate,
+        input_file_grp='OCR-D-GT-PAGE', # has wrong tokenisation but that's ok now
+        output_file_grp='OCR-D-LM-WORD',
+        parameter={'textequiv_level': 'word',
+                   'alternative_decoding': False,
+                   'model_file': MODEL},
+        **processor_kwargs
+    )
+    ws.save_mets()
+    n_pages = 0
+    n_words = 0
+    for file in ws.mets.find_files(fileGrp='OCR-D-LM-WORD'):
+        pcgts = page_from_file(ws.download_file(file))
+        metadata = pcgts.get_Metadata()
+        assert metadata
+        metadataitems = metadata.get_MetadataItem()
+        assert metadataitems
+        assert any([i for i in metadataitems
+                    if i.get_value() == 'ocrd-keraslm-rate'])
+        for line in pcgts.Page.get_AllTextLines():
+            for word in line.get_Word():
+                assert len(word.get_TextEquiv()) == 1 # only 1-best results
+                #for glyph in word.get_Glyph():
+                #    assert len(glyph.get_TextEquiv()) == 1 # only 1-best results
+                n_words += 1
+        n_pages += 1
+    assert n_pages > 1
+    assert n_words > 100
+    messages = [logrec.message for logrec in caplog.records]
+    assert "are off by" not in messages
+    assert len([msg for msg in messages if msg.startswith("Scoring text in page")]) == n_pages
+    assert all(float(dict([v.strip() for v in p.split(':')] for p in msg.split(','))['char ppl']) < 6.0
+               for msg in messages if msg.startswith("avg:"))
 
-    def setUp(self):
-        if os.path.exists(WORKSPACE_DIR):
-            shutil.rmtree(WORKSPACE_DIR)
-        os.makedirs(WORKSPACE_DIR)
-        initLogging()
-
-    def runTest(self):
-        resolver = Resolver()
-        workspace = resolver.workspace_from_url('test/assets/kant_aufklaerung_1784/mets.xml',
-                                                dst_dir=WORKSPACE_DIR, download=True)
-        self.assertIsNotNone(workspace)
-        #
-        # rate text alternative 1 on the word level:
-        #
-        KerasRate(
-            workspace,
-            input_file_grp='OCR-D-GT-PAGE', # has wrong tokenisation but that's ok now
-            output_file_grp='OCR-D-LM-WORD',
-            parameter={'textequiv_level': 'word',
-                       'alternative_decoding': False,
-                       'model_file': MODEL}
-            ).process()
-        workspace.save_mets()
-        for file in workspace.mets.find_files(fileGrp='OCR-D-LM-WORD'):
-            pcgts = page_from_file(workspace.download_file(file))
-            metadata = pcgts.get_Metadata()
-            self.assertIsNotNone(metadata)
-            metadataitems = metadata.get_MetadataItem()
-            self.assertIsNotNone(metadataitems)
-            rated = any([i for i in metadataitems if i.get_value() == 'ocrd-keraslm-rate'])
-            self.assertTrue(rated)
-        # 
-        # rate and viterbi-decode all text alternatives on the glyph level:
-        # 
-        TesserocrRecognize( # we need this to get alternatives to decode
-            workspace,
-            input_file_grp='OCR-D-GT-PAGE', # has wrong tokenisation but that's ok now
-            output_file_grp='OCR-D-OCR-TESS-GLYPH',
-            parameter={'textequiv_level': 'glyph',
-                       'overwrite_segments': True,
-                       'model': 'deu-frak'} # old model for alternatives
-            ).process()
-        workspace.save_mets()
-        KerasRate(
-            workspace,
-            input_file_grp='OCR-D-OCR-TESS-GLYPH',
-            output_file_grp='OCR-D-LM-GLYPH',
-            parameter={'textequiv_level': 'glyph',
-                       'alternative_decoding': True,
-                       'beam_width': 10, # not too slow
-                       'model_file': MODEL}
-            ).process()
-        workspace.save_mets()
-        for file in workspace.mets.find_files(fileGrp='OCR-D-LM-GLYPH'):
-            pcgts = page_from_file(workspace.download_file(file))
-            metadata = pcgts.get_Metadata()
-            self.assertIsNotNone(metadata)
-            metadataitems = metadata.get_MetadataItem()
-            self.assertIsNotNone(metadataitems)
-            rated = any([i for i in metadataitems if i.get_value() == 'ocrd-keraslm-rate'])
-            self.assertTrue(rated)
-            page = pcgts.get_Page()
-            for region in page.get_TextRegion():
-                for line in region.get_TextLine():
-                    for word in line.get_Word():
-                        for glyph in word.get_Glyph():
-                            self.assertEqual(len(glyph.get_TextEquiv()), 1) # only 1-best results
-
-if __name__ == '__main__':
-    main()
+def test_decoding(processor_kwargs, caplog):
+    """rate and viterbi-decode all text alternatives on the glyph level"""
+    caplog.set_level(logging.DEBUG)
+    ws = processor_kwargs['workspace']
+    run_processor(
+        TesserocrRecognize, # we need this to get alternatives to decode
+        input_file_grp='OCR-D-GT-PAGE', # has wrong tokenisation but that's ok now
+        output_file_grp='OCR-D-OCR-TESS-GLYPH',
+        parameter={'textequiv_level': 'glyph',
+                   'overwrite_segments': True,
+                   'model': 'deu-frak'}, # old model for alternatives
+        **processor_kwargs
+    )
+    ws.save_mets()
+    run_processor(
+        KerasRate,
+        input_file_grp='OCR-D-OCR-TESS-GLYPH',
+        output_file_grp='OCR-D-LM-GLYPH',
+        parameter={'textequiv_level': 'glyph',
+                   'alternative_decoding': True,
+                   'beam_width': 10, # not too slow
+                   'model_file': MODEL},
+        **processor_kwargs
+    )
+    ws.save_mets()
+    n_pages = 0
+    n_chars = 0
+    for file in ws.mets.find_files(fileGrp='OCR-D-LM-GLYPH'):
+        pcgts = page_from_file(ws.download_file(file))
+        metadata = pcgts.get_Metadata()
+        assert metadata
+        metadataitems = metadata.get_MetadataItem()
+        assert metadataitems
+        assert any([i for i in metadataitems
+                    if i.get_value() == 'ocrd-keraslm-rate'])
+        for line in pcgts.Page.get_AllTextLines():
+            for word in line.get_Word():
+                for glyph in word.get_Glyph():
+                    assert len(glyph.get_TextEquiv()) == 1 # only 1-best results
+                    n_chars += 1
+        n_pages += 1
+    assert n_pages > 1
+    assert n_chars > 1000
+    messages = [logrec.message for logrec in caplog.records]
+    assert "AssertionError" not in messages
+    assert len([msg for msg in messages if msg.startswith("Scoring text in page")]) == n_pages
+    #assert len([msg for msg in messages if msg.endswith("existing paths") and not msg.endswith("0 existing paths")]) > 0
+    assert all(float(dict([v.strip() for v in p.split(':')] for p in msg.split(','))['char ppl']) < 3.5
+               for msg in messages if msg.startswith("avg:"))
